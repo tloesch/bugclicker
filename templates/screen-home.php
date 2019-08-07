db1444c4ad036dcdc15d683b5760d626e4352386
MORHERO <m.rimkus1997@gmail.com>
2019-08-07 20:22:21 +0200

    add homescreen ascii

--- templates/screen-home.php
+++ templates/screen-home.php
@@ -45,11 +45,51 @@
     </div>
 
     <div id="workplace-panel" class="ascii flex wrap">
-      <p>
-        Welcome to Bugclicker v0.5 alpha.<br/>
-        In this version of the Game you will only find the very basics of the game.<br/>
-        Go to Github to view what will be added in the future: <a href="https://github.com/MORHERO/bugclicker" target="_blank" rel="nofollow">Github Repository</a>
-      </p>
+<p>
+  Welcome to Bugclicker v0.6 alpha.<br/>
+  In this version of the Game you will only find the very basics of the game.<br/>
+  Go to Github to view what will be added in the future: <a href="https://github.com/MORHERO/bugclicker" target="_blank" rel="nofollow">Github Repository</a>
+</p>
+         _______________________          ___________________________________________________
+        ||           |     |   `----------       | _______      ______      _______          |
+        ||___________|     |                     ||\______)   .´      `.   (_____ /|         |
+        |                 .|                     || |____)   |          |   (____| |         |
+        |                 |                      || |____)   |          |   (____| |         |
+        |---.             |                      ||/______)   `.______-´   (______\|         |
+        |   |             |                                                                  |
+        |(0)|             |                                                ________          |
+        |   |      ___     |                                              |        |         |
+        |---'    .´.-.`.   |                                              |     °  | ====||  |
+        |        |(   )|   |                                              | .´|  X |  .--||  |
+        |        `.`-´.´   |                     |                        ||  |  X | |   ||  |
+        |       ___`"´___  |                     |                        | `.|  X |  '--||  |
+        |______|_________|_|________          ___|                        |        | ====||  |
+        ||               |         `----------   |                        |        |         |
+        ||               |                       |                 ||==== |        |         |
+        ||_______________|                       |                 ||--.  | X  |`. |         |
+        |                                        |                 ||   | | X  |  ||         |
+        |                                        |                 ||--'  | X  |.´ |         |
+        |                                        |                 ||==== |  °     |         |
+        |                                        |                        |________|         |
+        |                                        |                                           |
+        |                                        |                                           |
+        |       ________________                 |          ________                         |
+        |      |                |           .-.  |         |        |                        |
+        |      |   ___          |          | O | |         |     °  | ====||                 |
+        |      |   |.|          |           '-'  |         | .´|  X |  .--||                 |
+        |      |_______         |       ________ |         ||  |  X | |   ||                 |
+        |              |        |      (______ /||         | `.|  X |  '--||                 |
+        |              |        |       (_____| ||         |        | ====||                 |
+        |      ||====  |        |       (_____| ||         |        |                        |
+        |      || .-.  | X  |`. |       (_____| ||  ||==== |        |                        |
+        |      ||(°_°) | X  |  ||       (_____| ||  ||--.  | X  |`. |                        |
+        |      || `-´  | X  |.´ |       (_____| ||  ||   | | X  |  ||                        |
+        |      ||====  |  °     |       (_____| ||  ||--'  | X  |.´ |                        |
+        |              |        |      (_______\||  ||==== |  °     |                        |
+        |              |________|                |         |________|                        |
+        |____[--------------]___[------------]___|_______[------------]__[------------]______|
+
+
     </div>
 
   </div>
